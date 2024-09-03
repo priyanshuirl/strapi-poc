269a6f9fe4cf49618bf4a60bd7e930e9f7563afb
Priyanshu Calyx <priyanshu12651265@gmail.com>
2024-09-03 17:19:42 +0530

    Link fix

--- config/plugins.js
+++ config/plugins.js
@@ -8,42 +8,42 @@ module.exports = {
         {
           uid: 'api::home.home',
           published: {
-            url: 'http://localhost:3000',
+            url: 'https://calyx-strapi-poc.netlify.app',
           },
         },
         {
           uid: 'api::page.page',
           draft: {
-            url: 'http://localhost:3000/api/preview',
+            url: 'https://calyx-strapi-poc.netlify.app/api/preview',
             query: {
               type: 'page',
               slug: '{slug}',
             },
           },
           published: {
-            url: 'http://localhost:3000/{slug}',
+            url: 'https://calyx-strapi-poc.netlify.app/{slug}',
           },
         },
         {
           uid: 'api::post.post',
           draft: {
-            url: 'http://localhost:3000/api/preview',
+            url: 'https://calyx-strapi-poc.netlify.app/api/preview',
             query: {
               type: 'post',
               slug: '{slug}',
             },
           },
           published: {
-            url: 'http://localhost:3000/blog/{slug}',
+            url: 'https://calyx-strapi-poc.netlify.app/blog/{slug}',
           },
         },
         {
             uid: 'api::blog-post.blog-post',
             draft: {
-              url: 'http://localhost:3000/blog/preview/{id}',
+              url: 'https://calyx-strapi-poc.netlify.app/blog/preview/{id}',
             },
             published: {
-              url: 'http://localhost:3000/blog/{id}',
+              url: 'https://calyx-strapi-poc.netlify.app/blog/{id}',
             },
           },
       ],
