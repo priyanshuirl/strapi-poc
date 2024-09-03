0c6f3334d56df55e36ded11e0ded39ee8e130fb2
Priyanshu Calyx <priyanshu12651265@gmail.com>
2024-09-03 17:03:13 +0530

    Added preview button

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,52 @@
-module.exports = () => ({});
+// ./config/plugins.js
+'use strict';
+
+module.exports = {
+  'preview-button': {
+    config: {
+      contentTypes: [
+        {
+          uid: 'api::home.home',
+          published: {
+            url: 'http://localhost:3000',
+          },
+        },
+        {
+          uid: 'api::page.page',
+          draft: {
+            url: 'http://localhost:3000/api/preview',
+            query: {
+              type: 'page',
+              slug: '{slug}',
+            },
+          },
+          published: {
+            url: 'http://localhost:3000/{slug}',
+          },
+        },
+        {
+          uid: 'api::post.post',
+          draft: {
+            url: 'http://localhost:3000/api/preview',
+            query: {
+              type: 'post',
+              slug: '{slug}',
+            },
+          },
+          published: {
+            url: 'http://localhost:3000/blog/{slug}',
+          },
+        },
+        {
+            uid: 'api::blog-post.blog-post',
+            draft: {
+              url: 'http://localhost:3000/blog/preview/{id}',
+            },
+            published: {
+              url: 'http://localhost:3000/blog/{id}',
+            },
+          },
+      ],
+    },
+  },
+};
